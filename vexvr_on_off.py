7ddcf1b35a4bd9e61261e9640ea1458857bd2085
Evan Weinberg <eweinberg@ssis.edu.vn>
2021-01-08 14:50:27 +0700

    Create vexvr_on_off.py
    Vexcode VR file additions

---
+++ vexvr_on_off.py
@@ -0,0 +1,41 @@
+# ------------------------------------------
+# 
+# 	Project:      VEXcode Project
+#	Author:       VEX
+#	Created:
+#	Description:  VEXcode VR Python Project
+# 
+# ------------------------------------------
+
+# Library imports
+from vexcode import *
+
+def driveControlOnOff(duration):
+    setpoint = 0
+    speed = 100
+    # reset the timer
+    brain.timer_reset()
+    # begin algorithm to drive straight to the position of y = 0
+
+    # loop while the timer is less than the duration input of the function.
+    while(brain.timer_time(SECONDS)<duration):
+        currentYLocation = location.position(Y,MM)
+        if( currentYLocation < setpoint):
+            drivetrain.drive(FORWARD)
+        elif (currentYLocation > setpoint):
+            drivetrain.drive(REVERSE)
+        else:
+            drivetrain.stop()
+
+        #VEXCode VR requires that we have a small pause in any loop we run.    
+        wait(1,MSEC)
+
+
+# Add project code in "main"
+def main():
+    driveControlOnOff(5)
+    drivetrain.turn_to_heading(90, DEGREES)
+
+
+# VR threads — Do not delete
+vr_thread(main())
