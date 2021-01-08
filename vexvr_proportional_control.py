28b7fd72ee10babcc97141eb7affad464f89591e
Evan Weinberg <eweinberg@ssis.edu.vn>
2021-01-08 14:54:40 +0700

    Create vexvr_proportional_control.py

---
+++ vexvr_proportional_control.py
@@ -0,0 +1,49 @@
+# ------------------------------------------
+# 
+# 	Project:      VEXcode Project
+#	Author:       VEX
+#	Created:
+#	Description:  VEXcode VR Python Project
+# 
+# ------------------------------------------
+
+# Library imports
+from vexcode import *
+
+def driveControlProportional(duration):
+    setpoint = 0
+    maxSpeed = 100
+    k = 1
+    # reset the timer
+    brain.timer_reset()
+    # begin algorithm to drive straight to the position of y = 0
+
+    # loop while the timer is less than the duration input of the function.
+    while(brain.timer_time(SECONDS)<duration):
+        currentYLocation = location.position(Y,MM)
+        error = setpoint - currentYLocation
+        output = k*error
+        # Ensure the output is not more than the maximum speed
+        if(output>maxSpeed):
+            output = maxSpeed
+        elif(output<-maxSpeed):
+            output = -maxSpeed
+        brain.print(output)
+        brain.new_line()
+        drivetrain.drive(FORWARD)
+        drivetrain.set_drive_velocity(output,PERCENT)
+        # Set the direction of movement
+        
+        #VEXCode VR requires that we have a small pause in any loop we run.    
+        wait(1,MSEC)
+    drivetrain.stop()
+
+
+
+# Add project code in "main"
+def main():
+    driveControlProportional(5)
+    drivetrain.turn_to_heading(90,DEGREES,wait=True)
+
+# VR threads — Do not delete
+vr_thread(main())
