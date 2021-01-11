858332872586c05e33fddecaf6ddc82ed333d39f
bandybam <75055234+bandybam@users.noreply.github.com>
2021-01-11 09:03:49 +0700

    Update task_1_template.py
    proportional control

--- vexvr/task_1_template.py
+++ vexvr/task_1_template.py
@@ -1,13 +1,45 @@
+# ------------------------------------------
+# 
+# 	Project:      VEXcode Project
+#	Author:       VEX
+#	Created:
+#	Description:  VEXcode VR Python Project
+# 
+# ------------------------------------------
+
 from math import *
 from random import randint
 
+
+
 def driveXDistance(setpoint,duration):
+    setpoint = 0
+    maxvel = 100
+    
     # reset the timer
     brain.timer_reset()
 
     # loop while the timer is less than the duration input of the function.
     while(brain.timer_time(SECONDS)<duration):
         # Your code goes here!
+        error = setpoint-location.position(X,MM)
+        k=1.5
+        drivespeed = error*k
+        if(drivespeed>maxvel):
+            drivespeed = maxvel
+        elif(drivespeed<-maxvel):
+            drivespeed = -maxvel
+        drivetrain.set_drive_velocity(drivespeed, PERCENT)
+        brain.print(drivespeed)
+        brain.new_line()
+        drivetrain.drive(FORWARD)
+
+        
+
+        
+
+        
+
         
 
 
@@ -23,18 +55,26 @@ def driveXDistance(setpoint,duration):
     drivetrain.stop()
 
 def driveYDistance(setpoint,duration):
+    setpoint = 0
+    maxvel = 100
+    
     # reset the timer
     brain.timer_reset()
 
     # loop while the timer is less than the duration input of the function.
     while(brain.timer_time(SECONDS)<duration):
         # Your code goes here!
-        
-
-
-
-
-
+        error = setpoint-location.position(Y,MM)
+        k=1.5
+        drivespeed = error*k
+        if(drivespeed>maxvel):
+            drivespeed = maxvel
+        elif(drivespeed<-maxvel):
+            drivespeed = -maxvel
+        drivetrain.set_drive_velocity(drivespeed, PERCENT)
+        brain.print(drivespeed)
+        brain.new_line()
+        drivetrain.drive(FORWARD)
 
 
 
@@ -45,13 +85,26 @@ def driveYDistance(setpoint,duration):
 
 
 def driveDiagonalDistance(setpoint,duration):
+    setpoint=400
+    maxvel=100
     # reset the timer
     brain.timer_reset()
 
     # loop while the timer is less than the duration input of the function.
     while(brain.timer_time(SECONDS)<duration):
         # Your code goes here!
-        
+        error = setpoint-location.position(Y,MM)
+        k=1.5
+        drivespeed = error*k
+        if(drivespeed>maxvel):
+            drivespeed = maxvel
+        elif(drivespeed<-maxvel):
+            drivespeed = -maxvel
+        drivetrain.set_drive_velocity(drivespeed, PERCENT)
+        brain.print(drivespeed)
+        brain.new_line()
+        drivetrain.drive(FORWARD)
+       
 
 
 
@@ -73,6 +126,6 @@ def main():
     drivetrain.turn_to_heading(0,DEGREES,wait=True)
     driveYDistance(0,3)
     drivetrain.turn_to_heading(45,DEGREES,wait=True)
-    driveDiagDistance(400,4)
+    driveDiagonalDistance(400,4)
 # VR threads — Do not delete
 vr_thread(main())
