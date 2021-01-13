287c4c598b1ae70f9a65875c75c05b6ba832fc34
bandybam <75055234+bandybam@users.noreply.github.com>
2021-01-13 14:18:24 +0700

    Update task_2_template.py
    my code for task 2

--- vexvr/task_2_template.py
+++ vexvr/task_2_template.py
@@ -1,10 +1,11 @@
 from math import *
 from random import randint
-
+xCoordinate = randint(-4,8)*100
+yCoordinate = randint(-4,8)*100
+maxvel = 100
 def generateRandomPoint():
-    xCoordinate = randint(-4,8)*100
-    yCoordinate = randint(-4,8)*100
     return [xCoordinate,yCoordinate]
+
 def driveXDistance(setpoint,duration):
     # reset the timer
     brain.timer_reset()
@@ -12,9 +13,17 @@ def driveXDistance(setpoint,duration):
     # loop while the timer is less than the duration input of the function.
     while(brain.timer_time(SECONDS)<duration):
         # Your code goes here!
-        
-
-
+        error = xCoordinate - location.position(X,MM)
+        k = 1.5
+        drivespeed = error*k
+        if(drivespeed>maxvel):
+            drivespeed = maxvel
+        elif(drivespeed<-maxvel):
+            drivespeed = -maxvel
+        drivetrain.set_drive_velocity(drivespeed, PERCENT)
+        brain.print(drivespeed)
+        brain.new_line()
+        drivetrain.drive(FORWARD)
 
 
 
@@ -33,7 +42,17 @@ def driveYDistance(setpoint,duration):
     # loop while the timer is less than the duration input of the function.
     while(brain.timer_time(SECONDS)<duration):
         # Your code goes here!
-        
+        error = yCoordinate - location.position(Y,MM)
+        k = 1.5
+        drivespeed = error*k
+        if(drivespeed>maxvel):
+            drivespeed = maxvel
+        elif(drivespeed<-maxvel):
+            drivespeed = -maxvel
+        drivetrain.set_drive_velocity(drivespeed, PERCENT)
+        brain.print(drivespeed)
+        brain.new_line()
+        drivetrain.drive(FORWARD)
 
 
 
@@ -55,7 +74,17 @@ def driveUsingDistanceSensor(setpoint,duration):
     # loop while the timer is less than the duration input of the function.
     while(brain.timer_time(SECONDS)<duration):
         # Your code goes here!
-        
+        error = distance.get_distance(MM) - setpoint
+        k=1.5
+        drivespeed = k*error
+        if(drivespeed>maxvel):
+            drivespeed = maxvel
+        elif(drivespeed<-maxvel):
+            drivespeed = -maxvel
+        drivetrain.set_drive_velocity(drivespeed, PERCENT)
+        brain.print(drivespeed)
+        brain.new_line()
+        drivetrain.drive(FORWARD)
 
 
 
@@ -77,10 +106,10 @@ def main():
     brain.print("target location is x = ( " + str(target[0]) + " , " + str(target[1]) + " )" )
     pen.move(DOWN)
     drivetrain.turn_to_heading(90,DEGREES,wait=True)
-    driveXDistance(target[0],4)
-    drivetrain.turn_to_heading(0,DEGREES,wait=True)
-    driveYDistance(target[1],4)
-    drivetrain.turn_to_heading(90,DEGREES,wait=True)
+    #driveXDistance(target[0],4)
+    #drivetrain.turn_to_heading(0,DEGREES,wait=True)
+    #driveYDistance(target[1],4)
+    #drivetrain.turn_to_heading(90,DEGREES,wait=True)
     driveUsingDistanceSensor(200,5)
 # VR threads — Do not delete
 vr_thread(main())
