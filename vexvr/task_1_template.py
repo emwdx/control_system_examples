6dd4d720dde3c7ec4dca88bf7d15465fa3efc888
TriGiang854 <75057397+TriGiang854@users.noreply.github.com>
2021-01-13 13:49:57 +0700

    tasks 1.2

--- vexvr/task_1_template.py
+++ vexvr/task_1_template.py
@@ -3,99 +3,89 @@ from random import randint
 
 def driveXDistance(setpoint,duration):
     setpoint = 0
-    drivetrain.set_drive_velocity(60, PERCENT)
-    tolerance = 5
+    maxSpeed = 100
+    k = 1
     # reset the timer
     brain.timer_reset()
+    # begin algorithm to drive straight to the position of y = 0
 
     # loop while the timer is less than the duration input of the function.
     while(brain.timer_time(SECONDS)<duration):
-        # Your code goes here!
-        currentYLocation = location.position(X,MM)
-        if( currentYLocation < setpoint - tolerance):
-            drivetrain.drive(FORWARD)
-        elif (currentYLocation > setpoint + tolerance):
-            drivetrain.drive(REVERSE)
-        else:
-            drivetrain.stop()
-
-
-
-
-
-
-
-
+        currentXLocation = location.position(X,MM)
+        error = setpoint - currentXLocation
+        output = k*error
+        # Ensure the output is not more than the maximum speed
+        if(output>maxSpeed):
+            output = maxSpeed
+        elif(output<-maxSpeed):
+            output = -maxSpeed
+        brain.print(output)
+        brain.new_line()
+        drivetrain.drive(FORWARD)
+        drivetrain.set_drive_velocity(output,PERCENT)
+        # Set the direction of movement
+        
         #VEXCode VR requires that we have a small pause in any loop we run.    
         wait(1,MSEC)
     drivetrain.stop()
 
 def driveYDistance(setpoint,duration):
     setpoint = 0
-    drivetrain.set_drive_velocity(60, PERCENT)
-
-   
-    tolerance = 5
+    maxSpeed = 100
+    k = 1
     # reset the timer
     brain.timer_reset()
+    # begin algorithm to drive straight to the position of y = 0
 
     # loop while the timer is less than the duration input of the function.
     while(brain.timer_time(SECONDS)<duration):
-        # Your code goes here!
         currentYLocation = location.position(Y,MM)
-        if( currentYLocation < setpoint - tolerance):
-            drivetrain.drive(FORWARD)
-        elif (currentYLocation > setpoint + tolerance):
-            drivetrain.drive(REVERSE)
-        else:
-            drivetrain.stop()
-
-
-
-
-
-
-
-
-
+        error = setpoint - currentYLocation
+        output = k*error
+        # Ensure the output is not more than the maximum speed
+        if(output>maxSpeed):
+            output = maxSpeed
+        elif(output<-maxSpeed):
+            output = -maxSpeed
+        brain.print(output)
+        brain.new_line()
+        drivetrain.drive(FORWARD)
+        drivetrain.set_drive_velocity(output,PERCENT)
+        # Set the direction of movement
+        
         #VEXCode VR requires that we have a small pause in any loop we run.    
         wait(1,MSEC)
     drivetrain.stop()
+
+
+
 def driveDiagDistance(setpoint,duration):
     setpoint = 400
-    drivetrain.set_drive_velocity(60, PERCENT)
-
-    
-    tolerance = 5
+    maxSpeed = 100
+    k = 1
     # reset the timer
     brain.timer_reset()
+    # begin algorithm to drive straight to the position of y = 0
 
     # loop while the timer is less than the duration input of the function.
     while(brain.timer_time(SECONDS)<duration):
-        # Your code goes here!
         currentYLocation = location.position(Y,MM)
-        if( currentYLocation < setpoint - tolerance):
-            drivetrain.drive(FORWARD)
-        elif (currentYLocation > setpoint + tolerance):
-            drivetrain.drive(REVERSE)
-        else:
-            drivetrain.stop()
-
-
-
-
-
-
-
-
-
-
-
-
+        error = setpoint - currentYLocation
+        output = k*error
+        # Ensure the output is not more than the maximum speed
+        if(output>maxSpeed):
+            output = maxSpeed
+        elif(output<-maxSpeed):
+            output = -maxSpeed
+        brain.print(output)
+        brain.new_line()
+        drivetrain.drive(FORWARD)
+        drivetrain.set_drive_velocity(output,PERCENT)
+        # Set the direction of movement
+        
         #VEXCode VR requires that we have a small pause in any loop we run.    
         wait(1,MSEC)
     drivetrain.stop()
-
 
 
 # Add project code in "main"
